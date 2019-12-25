da834d1c6f80b679244aabe066b8fedcf55f6a62
mRcfps <1402491442@qq.com>
2019-12-25 15:59:53 +0800

    Hooks 轻装上阵

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,63 +1,43 @@
-import Taro, { Component } from '@tarojs/taro'
+import Taro, { useState } from '@tarojs/taro'
 import { View } from '@tarojs/components'
 import { PostCard, PostForm } from '../../components'
 import './index.scss'
 
-export default class Index extends Component {
-  state = {
-    posts: [
-      {
-        title: '泰罗奥特曼',
-        content: '泰罗是奥特之父和奥特之母唯一的亲生儿子。',
-      },
-    ],
-    formTitle: '',
-    formContent: '',
-  }
+export default function Index() {
+  const [posts, setPosts] = useState([
+    {
+      title: '泰罗奥特曼',
+      content: '泰罗是奥特之父和奥特之母唯一的亲生儿子。',
+    },
+  ])
+  const [formTitle, setFormTitle] = useState('')
+  const [formContent, setFormContent] = useState('')
 
-  config = {
-    navigationBarTitleText: '首页',
-  }
-
-  handleSubmit(e) {
+  function handleSubmit(e) {
     e.preventDefault()
 
-    const { formTitle: title, formContent: content } = this.state
-    const newPosts = this.state.posts.concat({ title, content })
-
-    this.setState({
-      posts: newPosts,
-      formTitle: '',
-      formContent: '',
-    })
+    const newPosts = posts.concat({ title: formTitle, content: formContent })
+    setPosts(newPosts)
+    setFormTitle('')
+    setFormContent('')
   }
 
-  handleTitleInput(e) {
-    this.setState({
-      formTitle: e.target.value,
-    })
-  }
+  return (
+    <View className="index">
+      {posts.map((post, index) => (
+        <PostCard key={index} title={post.title} content={post.content} />
+      ))}
+      <PostForm
+        formTitle={formTitle}
+        formContent={formContent}
+        handleSubmit={e => handleSubmit(e)}
+        handleTitleInput={e => setFormTitle(e.target.value)}
+        handleContentInput={e => setFormContent(e.target.value)}
+      />
+    </View>
+  )
+}
 
-  handleContentInput(e) {
-    this.setState({
-      formContent: e.target.value,
-    })
-  }
-
-  render() {
-    return (
-      <View className="index">
-        {this.state.posts.map((post, index) => (
-          <PostCard key={index} title={post.title} content={post.content} />
-        ))}
-        <PostForm
-          formTitle={this.state.formTitle}
-          formContent={this.state.formContent}
-          handleSubmit={e => this.handleSubmit(e)}
-          handleTitleInput={e => this.handleTitleInput(e)}
-          handleContentInput={e => this.handleContentInput(e)}
-        />
-      </View>
-    )
-  }
+Index.config = {
+  navigationBarTitleText: '首页',
 }
