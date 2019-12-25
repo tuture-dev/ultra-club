856503595e639cacad7f08cba6408f0f12d32c7c
mRcfps <1402491442@qq.com>
2019-12-25 15:39:25 +0800

    起步

---
+++ src/pages/index/index.jsx
@@ -0,0 +1,17 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Text } from '@tarojs/components'
+import './index.scss'
+
+export default class Index extends Component {
+  config = {
+    navigationBarTitleText: '首页',
+  }
+
+  render() {
+    return (
+      <View className="index">
+        <Text>Hello world!</Text>
+      </View>
+    )
+  }
+}
