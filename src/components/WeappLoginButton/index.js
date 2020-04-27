29ce05bdb74dd53f6d83b82591186d2d1d10835b
Tom Huang <huan1043269994@gmail.com>
2020-04-27 11:00:21 +0800

    集成微信授权登录

--- src/components/WeappLoginButton/index.js
+++ src/components/WeappLoginButton/index.js
@@ -1,9 +1,10 @@
-import Taro, { useState } from '@tarojs/taro'
+import Taro, { useState, useEffect } from '@tarojs/taro'
 import { Button } from '@tarojs/components'
 import { useDispatch } from '@tarojs/redux'
 
 import './index.scss'
-import { LOGIN } from '../../constants'
+import { SET_LOGIN_INFO } from '../../constants'
+import Authing from '../../utils/authing/authing'
 
 export default function WeappLoginButton(props) {
   const [isLogin, setIsLogin] = useState(false)
@@ -13,15 +14,73 @@ export default function WeappLoginButton(props) {
   async function onGetUserInfo(e) {
     setIsLogin(true)
 
-    const { avatarUrl, nickName } = e.detail.userInfo
-    const userInfo = { avatar: avatarUrl, nickName }
-
-    dispatch({
-      type: LOGIN,
-      payload: {
-        userInfo: userInfo,
-      },
+    const userPoolId = '5ea4ffa72b3a80b6eff60b65'
+    const authing = new Authing({
+      userPoolId,
     })
+
+    async function loginWithAuthing(code, detail) {
+      try {
+        const userInfo = await authing.loginWithWxapp({
+          code,
+          detail,
+        })
+
+        // 提示登录成功
+        Taro.atMessage({
+          type: 'success',
+          message: '恭喜您，登录成功！',
+        })
+
+        const { nickname, photo, _id } = userInfo
+
+        dispatch({
+          type: SET_LOGIN_INFO,
+          payload: { nickName: nickname, avatar: photo, userId: _id },
+        })
+
+        // 向后端发起登录请求
+        await Taro.setStorage({
+          key: 'userInfo',
+          data: {
+            nickName: nickname,
+            avatar: photo,
+            _id,
+          },
+        })
+
+        await Taro.setStorage({
+          key: 'token',
+          data: userInfo.token,
+        })
+
+        // 当 code 用于登录之后，会失效，所以这里重新获取 code
+        Taro.login({
+          success(res) {
+            const code = res.code
+            Taro.setStorageSync('code', code)
+          },
+        })
+      } catch (err) {
+        console.log('err', err)
+        Taro.atMessage({
+          type: 'success',
+          message: '恭喜您，登录成功！',
+        })
+      }
+    }
+
+    try {
+      const code = Taro.getStorageSync('code')
+      Taro.login({
+        success(res) {
+          const code = res.code
+          loginWithAuthing(code, e.detail)
+        },
+      })
+    } catch (err) {
+      console.log('err', err)
+    }
 
     setIsLogin(false)
   }
